4d3b806f0921bc9cd87ec72a68156c4f5f749300
SiriAung <thirinandaraung614@gmail.conm>
2023-02-23 16:12:41 +0630

    first commit

---
+++ server/routes/user.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+// IMPORT ALL THE CONTROLLERS ...
+import { createUser, getAllUsers, getUserInfoByID } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.route("/").get(getAllUsers);
+router.route("/").post(createUser);
+router.route("/:id").get(getUserInfoByID);
+
+export default router;
